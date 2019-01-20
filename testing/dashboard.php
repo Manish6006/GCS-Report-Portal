7925d4f5a0ef7ef0511b16eb884f1e6669bfdc45
Manish Kumar Sharma <manish.sharma591@gmail.com>
2019-01-20 17:06:30 +0530

    Dashboard Page Added

---
+++ testing/dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+<link rel="stylesheet" type="text/css" href="outage.css">
+
+</head>
+<body>
+
+
+<body id="body" style="height: 100%;"  data-gr-c-s-loaded="true">
+
+	<div class="se-pre-con" style="display: none;"></div>
+
+	<div class="HeadBanner" style=" box-shadow: 6px 6px 2px grey;">
+		
+		<div class="container-fluid">
+			<div class="row" style="background-color: #E31837; color: white;">
+				
+				<div class="col-sm-4" style=" padding-bottom: 5px;">
+					<img src="img/trans_white_mahindralogo.png" style="width: 200px; height: 50px;">
+				</div>
+					<div class="col-sm-4 text-center" style="font-size: 23pt; padding-top: 3px; padding-bottom: 5px; text-shadow: 2px 2px 4px #000000;">
+						<span style="height: 40px;"> DASHBOARD</span>
+					</div>
+
+					<div class="col-sm-4" style="font-size: 13pt; padding-top: 15px; text-align: right; 
+						padding-right: 15px;
+					padding-bottom: 5px;">
+						
+						<span style="height: 40px;">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Welcome to <?php session_start(); echo $_SESSION['usr']?>&nbsp;&nbsp;&nbsp;|&nbsp;
+							<a href="logout.php" style="color:white;"> Logout</a>
+						</span>
+					</div>
+			</div>
+			
+		</div>
+	</div>
+
+</body>
+</html>
--- testing/newindex.php
+++ testing/newindex.php
@@ -9,7 +9,27 @@ if(isset($_POST['login'])) {
     if(empty($_POST['Uname'])|| empty($_POST['Passwd'])){
 
       $error="Username or Password is invalid";
-    }
+
+    }else if ($_POST['Uname'] =="pala.anjaneyulu"  &&  $_POST['Passwd']=="pala"){
+       $username=$_POST['Uname'];
+       $password=$_POST['Passwd'];
+        $conn=mysqli_connect("localhost","root","");
+
+        $db=mysqli_select_db($conn,"cc");
+
+        $query=mysqli_query($conn, "SELECT * FROM logintable WHERE username='$username' AND password='$password'");
+          
+        $rows=mysqli_num_rows($query);
+
+        if($rows==1){
+          //echo "logged in successfully!!!!!!"." "." ".$_POST['Uname'];
+          
+                header("location:dashboard.php");
+                $_SESSION['usr']=$username;
+            } else{
+                     $error="Username or Password is invalid";
+                } 
+       }           
     else{
 
       $username=$_POST['Uname'];
